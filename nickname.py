cb01299b3d72d387606a0c30831abf7672616bc4
gqmv <gqmv@cin.ufpe.br>
2020-03-11 20:45:40 -0300

    Adicao da questao Nickname

---
+++ nickname.py
@@ -0,0 +1,9 @@
+# Obtem os valores de entrada
+primeiro_nome = input()
+segundo_nome = input()
+
+# Realiza a juncao dos nomes
+nickname = f"{primeiro_nome}{segundo_nome}"
+
+# Informa o nickname
+print(nickname)
